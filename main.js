1639815e4c16349b4c27ecccc54fab0db8e08210
GianmarcoMontanari <giammont@live.it>
2020-04-08 12:33:58 +0200

    Aggiunta della timing function con risposta

--- main.js
+++ main.js
@@ -22,25 +22,60 @@ $( document ).ready(function() {
       var testoRisposta = $("#inputRisposta").val();
       // ritrasformo la variabile nel div da inserire dopo gli altri messaggi
       if (testoRisposta != ""){
-        var dt = new Date();
-        var time = dt.getHours() + ":" + dt.getMinutes()
-        var elementoRisposta =$(
+        // prendo l'orario della macchina tramite funzione globale
+        time();
+
+        // creo la variabile che contiene il blocco html da stampare
+        var rispostaInviata =$(
         "<div class='messaggio inviato'>" +
           "<span class=\"testo-messaggio\"></span>"+
           "<span class=\"freccetta-info\"><i class=\"fas fa-chevron-down\"></i></span>"+
           "<span class=\"orario-messaggio\"></span>" +
         "</div>");
 
-        elementoRisposta.children(".testo-messaggio").text(testoRisposta);
-        elementoRisposta.children(".orario-messaggio").text(time);
-
+        rispostaInviata.children(".testo-messaggio").text(testoRisposta);
+        rispostaInviata.children(".orario-messaggio").text(time);
 
         // selezionando il contenitore della chat, "appendo" ad esso il nuovo div
-        $("#chat").append(elementoRisposta);
+        $("#chat").append(rispostaInviata);
         // console.log(testoRisposta);
         $("#inputRisposta").val("");
+
+        // impostando il timeout a 1 secondo, rischiamo la funzione che mi stamnpa la risposta
+        setTimeout(rispostaRicevuta, 1000);
+
 
       } // fine ciclo if di controllo contenuto input
     } //fine ciclo if di controllo tasto premuto
   }); // fine evento keypress
 }); // fine document.ready
+
+
+
+
+
+// -----------------------------FUNZIONI GLOBALI ---------------------------------------
+
+// funzione per il ricevimento della risposta
+function rispostaRicevuta(){
+
+  var messaggioRicevuto =$(
+    "<div class='messaggio ricevuto'>" +
+    "<span class=\"testo-messaggio\"></span>"+
+    "<span class=\"freccetta-info\"><i class=\"fas fa-chevron-down\"></i></span>"+
+    "<span class=\"orario-messaggio\"></span>" +
+  "</div>");
+
+  time();
+
+  messaggioRicevuto.children(".testo-messaggio").text("ok");
+  messaggioRicevuto.children(".orario-messaggio").text(time);
+  $("#chat").append(messaggioRicevuto);
+}
+// --------------------------------------------------------------
+// funzione per l'ottenimento del tempo attuale
+function time(){
+  var dt = new Date();
+  var time = dt.getHours() + ":" + dt.getMinutes();
+  return time;
+}
